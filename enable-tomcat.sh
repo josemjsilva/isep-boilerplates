e12e9744da041ea69f90c507ea590f8ba201cb00
José Silva <32419017+zesilva15@users.noreply.github.com>
2023-03-02 00:36:36 +0000

    Update enable-tomcat.sh

--- enable-tomcat.sh
+++ enable-tomcat.sh
@@ -1,7 +1,7 @@
 sudo apt update && sudo apt install default-jdk -y
 sudo useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
 curl -O https://downloads.apache.org/tomcat/tomcat-9/v9.0.72/bin/apache-tomcat-9.0.72.tar.gz
-mv apache-tomcat-9.0.72.tar.gz /tmp/apache-tomcat-9.0.72.tar.gz
+sudo mv apache-tomcat-9.0.72.tar.gz /tmp/apache-tomcat-9.0.72.tar.gz
 sudo tar xf /tmp/apache-tomcat-9*.tar.gz -C /opt/tomcat
 sudo mv /opt/tomcat/apache-tomcat-9.0.72 /opt/tomcat/latest
 sudo chown -R tomcat: /opt/tomcat/latest
